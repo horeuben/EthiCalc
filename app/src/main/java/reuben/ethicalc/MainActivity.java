2e4ab8580516009b067011d7c3621b808e4cca53
Reuben <reuben_ho@hotmail.com>
2017-12-06 00:17:42 +0800

    Added login to firebase
    enabled firebase login to app via:
    -email
    -google account
    -facebook

--- app/src/main/java/reuben/ethicalc/MainActivity.java
+++ app/src/main/java/reuben/ethicalc/MainActivity.java
@@ -1,13 +1,29 @@
 package reuben.ethicalc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+    private Facebook
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
     }
+
 }
