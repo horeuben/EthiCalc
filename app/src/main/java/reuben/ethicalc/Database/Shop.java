e1f0b4a8ed44c8d6cd4228da346a707b29bc3dc1
Reuben <reuben_ho@hotmail.com>
2017-12-06 21:34:13 +0800

    Added lat lng to shop
    missing lat lng, added to Shop

--- app/src/main/java/reuben/ethicalc/Database/Shop.java
+++ app/src/main/java/reuben/ethicalc/Database/Shop.java
@@ -8,6 +8,24 @@ public class Shop {
     private String shopname;
     private String description;
     private String picureurl;
+    public String lat;
+    public String lng;
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
 
     public String getShopname() {
         return shopname;
