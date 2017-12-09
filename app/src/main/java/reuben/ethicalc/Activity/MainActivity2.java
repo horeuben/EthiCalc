8cb55f75565a5c98a0cbfdd99f20b8106006d168
Reuben <reuben_ho@hotmail.com>
2017-12-10 01:26:03 +0800

    Share photo button now works
    It is necessary for facebook native app to be installed according to
    facebook guidelines

--- app/src/main/java/reuben/ethicalc/Activity/MainActivity2.java
+++ app/src/main/java/reuben/ethicalc/Activity/MainActivity2.java
@@ -2,6 +2,10 @@ package reuben.ethicalc.Activity;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
@@ -11,9 +15,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareButton;
 import com.facebook.share.widget.ShareDialog;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -44,9 +53,9 @@ public class MainActivity2 extends AppCompatActivity  implements LocationListene
     private FirebaseAuth auth;
     private FirebaseUser user;
     private ShareDialog shareDialog;
-    private Button fbButton, logoutButton,testButton;
+    private Button fbshare, logoutButton,testButton;
     private CallbackManager callbackManager;
-
+    private TextView sharedContent;
     public static final String KEY = "Geofence";
     PendingIntent mGeofencePendingIntent;
     public static final int CONNECTION_FAILURE_RESOLUTION_REQUEST = 100;
@@ -68,25 +77,45 @@ public class MainActivity2 extends AppCompatActivity  implements LocationListene
         auth = FirebaseAuth.getInstance();
         user = FirebaseAuth.getInstance().getCurrentUser();
         shareDialog = new ShareDialog(this);
+        sharedContent = (TextView) findViewById(R.id.meme);
+        sharedContent.setText("My impact factor is ");
+        sharedContent.setBackground(getResources().getDrawable(R.drawable.pic));
         callbackManager = CallbackManager.Factory.create();
-        fbButton = (Button)findViewById(R.id.button_fb);
+
+        fbshare = (Button)findViewById(R.id.button_fbshare);
+
         logoutButton = (Button)findViewById(R.id.button_logout);
         testButton = (Button)findViewById(R.id.button_test);
-        fbButton.setOnClickListener(new View.OnClickListener() {
+
+        fbshare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (ShareDialog.canShow(ShareLinkContent.class)) {
-                    ShareLinkContent linkContent = new ShareLinkContent.Builder()
-                            .setContentTitle("Using EthiCalc!")
-                     //     .setImageUrl(Uri.parse("okLoginButton-min-300x136.png"))
-                            .setContentDescription("Hey whats up?")
-                          .setContentUrl(Uri.parse("www.google.com"))
-
-                            .build();
-                    shareDialog.show(linkContent);  // Show facebook ShareDialog
+                if(ShareDialog.canShow(ShareLinkContent.class)){
+                Bitmap image = getBitmapFromView(sharedContent);
+                SharePhoto photo = new SharePhoto.Builder()
+                        .setBitmap(image)
+                        .setCaption("Hey how are you?")
+                        .build();
+                SharePhotoContent content = new SharePhotoContent.Builder()
+                        .addPhoto(photo)
+                        .build();
+                shareDialog.show(content);
+//                    if (ShareDialog.canShow(ShareLinkContent.class)) {
+//                        ShareLinkContent linkContent = new ShareLinkContent.Builder()
+//                                .setContentTitle("Using EthiCalc!")
+//                                //     .setImageUrl(Uri.parse("okLoginButton-min-300x136.png"))
+//                                .setContentDescription("Hey whats up?")
+//                                .setContentUrl(Uri.parse("www.google.com"))
+//
+//                                .build();
+//                        shareDialog.show(linkContent);  // Show facebook ShareDialog
+//                    }
+               } else{
+                    Toast.makeText(MainActivity2.this,"You need facebook installed!",Toast.LENGTH_LONG).show();
                 }
             }
         });
+
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -261,6 +290,27 @@ public class MainActivity2 extends AppCompatActivity  implements LocationListene
             e.printStackTrace();
         }
 
+    }
+
+    // we have a textview with a background as image, and we want to create a bitmap through it to share from facebook when using
+    public static Bitmap getBitmapFromView(View view) {
+        //Define a bitmap with the same size as the view
+
+        Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),Bitmap.Config.ARGB_8888);
+        //Bind a canvas to it
+        Canvas canvas = new Canvas(returnedBitmap);
+        //Get the view's background
+        Drawable bgDrawable =view.getBackground();
+        if (bgDrawable!=null)
+            //has background drawable, then draw it on the canvas
+            bgDrawable.draw(canvas);
+        else
+            //does not have background drawable, then draw white background on the canvas
+            canvas.drawColor(Color.WHITE);
+        // draw the view on the canvas
+        view.draw(canvas);
+        //return the bitmap
+        return returnedBitmap;
     }
 
 
