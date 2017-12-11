9aaa6c76f0cd2d5c206196feea325d7c3b3a9772
Reuben <reuben_ho@hotmail.com>
2017-12-11 18:58:44 +0800

    Added business and impactfragment

--- app/src/main/java/reuben/ethicalc/Activity/MainActivity.java
+++ app/src/main/java/reuben/ethicalc/Activity/MainActivity.java
@@ -39,13 +39,14 @@ import com.squareup.picasso.Picasso;
 
 import reuben.ethicalc.Fragment.BlankFragment;
 import reuben.ethicalc.Fragment.GetNearbyShopsFragment;
+import reuben.ethicalc.Fragment.ImpactFragment;
 import reuben.ethicalc.Fragment.NewsFeedFragment;
 import reuben.ethicalc.R;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, BlankFragment.OnFragmentInteractionListener,GetNearbyShopsFragment.OnFragmentInteractionListener,NewsFeedFragment.OnFragmentInteractionListener {
+        implements NavigationView.OnNavigationItemSelectedListener, BlankFragment.OnFragmentInteractionListener,GetNearbyShopsFragment.OnFragmentInteractionListener,NewsFeedFragment.OnFragmentInteractionListener,ImpactFragment.OnFragmentInteractionListener {
     private FirebaseUser user;
     private ImageView imageViewProfilePic, imageViewStarIcon;
 
@@ -223,7 +224,7 @@ public class MainActivity extends AppCompatActivity
             scanIntegrator.setOrientationLocked(false);
             scanIntegrator.initiateScan();
         } else if (id == R.id.nav_impact) {
-            fragment = new BlankFragment();
+            fragment = new ImpactFragment();
 
         } else if (id == R.id.nav_share) {
             fragment = new GetNearbyShopsFragment();
